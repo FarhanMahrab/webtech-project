22a7e614257c533262c4451ec8f0de95052d08ac
Swapnil <swapnil7kuri@gmail.com>
2026-01-19 19:29:08 +0600

    db updated

--- Hotel_Management/db.php
+++ Hotel_Management/db.php
@@ -1,13 +1,14 @@
 <?php
-// db.php
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $pass = "";
-$dbname = "hotel_system";
+$db   = "hotel_system";
+$port = 3307; // <-- IMPORTANT: use the same port shown in phpMyAdmin (your screenshot shows 3307)
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli("127.0.0.1", "root", "", "hotel_system", 3307);
+
 
 if ($conn->connect_error) {
-  die("DB Connection failed: " . $conn->connect_error);
+    die("DB Connection failed: " . $conn->connect_error);
 }
 ?>
